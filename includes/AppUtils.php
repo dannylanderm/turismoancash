2d071cbb7885da82ea846de3e17d137d1a333d48
Maykol <m.cruzlopez@hotmail.com>
2019-12-18 00:38:01 -0500

    Permite registrar y listar lugar turistico. Cruz Lopez M.

--- includes/AppUtils.php
+++ includes/AppUtils.php
@@ -374,4 +374,51 @@
 		$newdate = new DateTime($hoy);
 		return date_format($newdate, 'd/m/Y');
 	}
-
+	
+	function DMStoDEC($deg, $min, $sec) {
+		// Converts DMS ( Degrees / minutes / seconds )
+		// to decimal format longitude / latitude
+		return $deg + ((($min * 60) + ($sec)) / 3600);
+	}
+	
+	function DECtoDMS2($dec) {
+		// Converts decimal longitude / latitude to DMS
+		// ( Degrees / minutes / seconds )
+		// This is the piece of code which may appear to
+		// be inefficient, but to avoid issues with floating
+		// point math we extract the integer part and the float
+		// part by using a string function.
+		$vars   = explode(".", $dec);
+		$deg    = $vars[0];
+		$tempma = "0.".$vars[1];
+		$tempma = $tempma * 3600;
+		$min    = floor($tempma / 60);
+		$sec    = $tempma - ($min * 60);
+		return ["deg" => $deg, "min" => $min, "sec" => $sec];
+	}
+	
+	function DECtoDMS($coord, $format = false) {
+		$isnorth = $coord >= 0;
+		$coord   = abs($coord);
+		$deg     = floor($coord);
+		$coord   = ($coord - $deg) * 60;
+		$min     = floor($coord);
+		$sec     = floor(($coord - $min) * 60);
+		$dir     = $isnorth ? 'N' : 'S';
+		
+		if (!$format) {
+			return ["deg" => $deg, "min" => $min, "sec" => $sec, "dir" => $dir];
+		} else {
+			return "{$deg}°{$min}'{$sec}\"{$dir}";
+		}
+	}
+	
+	function FormatCoord($coord) {
+		$deg = $coord['deg'];
+		$min = $coord['min'];
+		$sec = $coord['sec'];
+		$dir = $coord['dir'];
+		return "{$deg}°{$min}'{$sec}\"{$dir}";
+	}
+	
+	
